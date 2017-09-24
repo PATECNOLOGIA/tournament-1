32e755e8d671b0971be697b93f197ce12c211156
Armando Pitotti <armando.pitotti@gmail.com>
2017-09-24 11:20:58 +0200

    test

--- Trial/Program.cs
+++ Trial/Program.cs
@@ -12,25 +12,37 @@ namespace Trial
         {
             List<int> result = new List<int>();
             List<int> result1 = new List<int>();
+            List<int> result2 = new List<int>();
             List<string> binaries = new List<string>();
             Dictionary<int, int> elementsBinary = new Dictionary<int, int>();
             for(int ndx = 0; ndx < elements.Length; ndx++)
             {
                 string binary = Convert.ToString(elements[ndx], 2);
-                binaries.Add(binary);
+                //binaries.Add(binary);
                 elementsBinary.Add(elements[ndx], binary.Count(c => c == '1'));
             }
-            foreach(var item in elementsBinary.OrderBy(o => o.Value))
-            {             
-                result.Add(item.Key);
-            }
+            //foreach(var item in elementsBinary.OrderBy(o => o.Value))
+            //{                             
+            //    result.Add(item.Key);
+            //}
 
-            for (int ndx = 0; ndx < elementsBinary.Count; ndx++)
+            //result1 = elementsBinary.OrderBy(o => o.Key).Select(s => s.Key).ToList();
+
+            var orderedElementsBinary = elementsBinary.OrderBy(o => o.Value).ToDictionary(f => f.Key, f => f.Value);
+            int binaryValue = 0;
+            for (int ndx = 0; ndx < orderedElementsBinary.Count; ndx++)
             {
-                var item = elementsBinary.ElementAt(ndx);
-                if(ndx > 0 && item.Value == elementsBinary.ElementAt(ndx-1).Value)
+                var item = orderedElementsBinary.ElementAt(ndx);
+                if (ndx == 0) binaryValue = item.Value;
+                if (ndx > 0 && item.Value > binaryValue)
                 {
-
+                    binaryValue = item.Value;
+                    result1.Add(item.Key);
+                }
+                else
+                {
+                    binaryValue = item.Value;
+                    result2.Add(item.Key);
                 }
             }
             return result.ToArray();
